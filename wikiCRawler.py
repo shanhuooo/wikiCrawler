959866dc33a8fdd45f08535baa4ff961417f965c
shanhuooo <932915490@qq.com>
2019-08-11 09:53:39 +0800

    changed html

--- wikiCRawler.py
+++ wikiCRawler.py
@@ -2,7 +2,7 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
-html = urlopen("http://en.wikipedia.org/wiki/Kevin_Bacon")
+html = urlopen("https://wikipedia.hk.wjbk.site/wiki/Apple")
 bsObj = BeautifulSoup(html)
 for link in bsObj.findAll("a"):
 	if 'href' in link.attrs:
